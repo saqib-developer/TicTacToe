fbd459da17236eccafa7edaf3d0b339caeafe1a7
Muhammad Saqib Nawaz <79707059+saqibsaqib123@users.noreply.github.com>
2023-03-12 19:47:47 +0500

    Saqib

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,234 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let player = 1;
+  let ai = false;
+  let canrun = true;
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+
+  const turn = (element) => {
+    if (ai) {
+      let possible = element.hasAttribute('name');
+      if (!possible && canrun) {
+        element.setAttribute('name', 'checked');
+        element.firstElementChild.textContent = 'X';
+        element.firstElementChild.setAttribute('class', 'cross');
+        board[parseInt(element.id)] = "X";
+        computerTurn();
+        RealWinner();
+      }
+    } else {
+      let possible = element.hasAttribute('name');
+      if (!possible && canrun) {
+        element.setAttribute('name', 'checked');
+        if (player === 1) {
+          player++;
+          element.firstElementChild.textContent = 'X';
+          element.firstElementChild.setAttribute('class', 'cross');
+          board[parseInt(element.id)] = "X";
+        } else if (player === 2) {
+          player--;
+          element.firstElementChild.textContent = 'O';
+          element.firstElementChild.setAttribute('class', 'circle');
+          board[parseInt(element.id)] = "O";
+        }
+        RealWinner();
+
+      }
+    }
+  }
+
+  function computerTurn() {
+    let numb = minimax(board, 'O').move;
+    if (numb !== undefined) {
+      board[numb] = "O";
+      document.getElementById(numb.toString()).setAttribute('name', 'checked');
+      document.getElementById(numb.toString()).firstElementChild.textContent = 'O';
+      document.getElementById(numb.toString()).firstElementChild.setAttribute('class', 'circle');
+    }
+  }
+
+
+  function minimax(board, player) {
+    // Base case: check if the game is over
+    let result = checkWinner(board).winnerMark;
+    if (result !== null) {
+      // Return a value based on who won
+      switch (result) {
+        case "X": return { value: 10 };
+        case "O": return { value: -10 };
+        case "tie": return { value: 0 };
+        default: break;
+      }
+    }
+
+    // Recursive case: evaluate all possible moves
+    let bestMove;
+    let bestValue = (player === "X") ? -Infinity : Infinity; // Initialize best value
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "") {
+        // Make a move and get its value
+        board[i] = player;
+        let moveValue = minimax(board, (player === "X") ? "O" : "X").value;
+        board[i] = ""; // Undo the move
+
+        // Update best value and best move
+        if ((player === "X" && moveValue > bestValue) || (player === "O" && moveValue < bestValue)) {
+          bestValue = moveValue;
+          bestMove = i;
+        }
+      }
+    }
+
+    // Base case: return a value if there are no empty squares left
+    if (bestMove === undefined) {
+      return { value: 0 };
+    }
+
+    // Return the best move and its value
+    return { move: bestMove, value: bestValue };
+  }
+
+  function checkWinner(board) {
+    // Check rows
+    // 0 3 6
+    for (let i = 0; i < 9; i += 3) {
+      if (board[i] !== "" && board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        if (i === 6) {
+          return { winnerMark: board[i], place: 5 };
+        } else {
+          return { winnerMark: board[i], place: (i) };
+        }
+      }
+    }
+
+    // Check columns
+    // 0 1 2
+    for (let i = 0; i < 3; i++) {
+      if (board[i] !== "" && board[i] === board[i + 3] && board[i] === board[i + 6]) {
+        if (i === 2) {
+          return { winnerMark: board[i], place: 4 };
+        } else {
+          return { winnerMark: board[i], place: (i + 1) };
+        }
+      }
+    }
+
+    // Check diagonals
+    if (board[0] !== "" && board[0] === board[4] && board[0] === board[8]) {
+      return { winnerMark: board[0], place: 6 };
+    }
+    if (board[2] !== "" && board[2] === board[4] && board[2] === board[6]) {
+      return { winnerMark: board[2], place: 7 };
+    }
+
+    // Check for tie
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        return { winnerMark: null, place: null };
+      }
+    }
+    return { winnerMark: "tie", place: undefined };
+    ;
+  }
+
+
+  function RealWinner() {
+    let winnerPlace = checkWinner(board);
+
+    if (winnerPlace.winnerMark !== null) {
+      switch (winnerPlace.winnerMark) {
+        case "X": won('X'); break;
+        case "O": won('O'); break;
+        case "tie": draw(); break;
+        default: break;
+      }
+      if (winnerPlace.winnerMark !== 'tie') {
+        document.getElementById('line').style.display = 'block';
+        switch (winnerPlace.place) {
+          case 0:
+            console.log(0);
+            document.getElementById('line').style.transform = 'rotate(90deg)'
+            document.getElementById('line').style.marginBottom = '226px'
+            break;
+          case 1:
+            console.log(1);
+            document.getElementById('line').style.transform = 'rotate(0deg)'
+            document.getElementById('line').style.marginRight = '250px'
+            break;
+          case 2:
+            console.log(2);
+            document.getElementById('line').style.transform = 'rotate(0deg)'
+            break;
+          case 3:
+            console.log(3);
+            document.getElementById('line').style.transform = 'rotate(90deg)'
+            document.getElementById('line').style.marginBottom = '-27px'
+            break;
+          case 4:
+            console.log(4);
+            document.getElementById('line').style.transform = 'rotate(0deg)'
+            document.getElementById('line').style.marginRight = '-250px'
+            break;
+          case 5:
+            console.log(5);
+            document.getElementById('line').style.transform = 'rotate(90deg)'
+            document.getElementById('line').style.marginBottom = '-317px'
+            break;
+          case 6:
+            console.log(6);
+            document.getElementById('line').style.transform = 'rotate(-45deg)'
+            break;
+          case 7:
+            console.log(7);
+            document.getElementById('line').style.transform = 'rotate(45deg)'
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  function won(winner) {
+    document.getElementById('result').innerHTML = winner + ' Won';
+    canrun = false;
+  }
+
+  function draw() {
+    document.getElementById('result').innerHTML = 'Match Drawn';
+    canrun = false;
+  }
+
+  const restart = () => {
+    window.location.reload(false);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p id="result"></p>
+      <div className="cantainer">
+        <div className="line" id="line"></div>
+        <div className="rows">
+          <button className="box row-1 coloumn-1" id="0" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-1 coloumn-2" id="1" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-1 coloumn-3" id="2" onClick={(event) => { turn(event.target) }}><span></span></button>
+        </div>
+        <div className="rows">
+          <button className="box row-2 coloumn-1" id="3" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-2 coloumn-2" id="4" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-2 coloumn-3" id="5" onClick={(event) => { turn(event.target) }}><span></span></button>
+        </div>
+        <div className="rows">
+          <button className="box row-3 coloumn-1" id="6" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-3 coloumn-2" id="7" onClick={(event) => { turn(event.target) }}><span></span></button>
+          <button className="box row-3 coloumn-3" id="8" onClick={(event) => { turn(event.target) }}><span></span></button>
+        </div>
+      </div>
+      <button className="restart" onClick={restart}>Restart</button>
+    </>
   );
 }
 
